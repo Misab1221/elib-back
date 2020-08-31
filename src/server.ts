45792bb1690bc72bfdc6eeb8c2650b82105aa206
misabktvr@gmail.com <Alwin>
2020-08-31 20:48:56 +0530

    f

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app=express();
+const port=process.env.PORT||5000;
+app.get('/',(req,res)=>{
+    res.send("samples!!")
+});
+app.listen(port,()=>(err:any)=>{
+    if(err){
+        return console.error(err);
+    }
+    else
+    {
+        return console.log("Server started on port : "+port);
+    }
+});
